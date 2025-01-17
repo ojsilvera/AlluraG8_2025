e636d535782ffac1cc58582696798e2866e513fa
ojsc <ojsilvera@gmail.com>
2025-01-17 18:19:08 -0500

    Introduccion al poryecto del curso

---
+++ Fase_1/AdivinaNumero/app.js
@@ -0,0 +1,6 @@
+/*
+nos permite mostrar una alerta en el navegador con la frase hola mundo, se encuentra emtre "" para
+designar una cadena de texto, tambien se puede reemplazar con una variable que contenga un numero o
+un texto
+*/
+alert("Hola mundo");
