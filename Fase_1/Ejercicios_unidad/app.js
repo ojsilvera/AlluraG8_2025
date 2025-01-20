3c10796d7ce3dc949e8cb666867f590008f02943
ojsc <ojsilvera@gmail.com>
2025-01-20 16:08:07 -0500

    ejercicios de parctica 2

--- Fase_1/Ejercicios_unidad/app.js
+++ Fase_1/Ejercicios_unidad/app.js
@@ -49,7 +49,7 @@ if (edadUsuario >= 18) {
 /* 1. Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!".
 De lo contrario, muestra "¡Buena semana!". */
 
-let hoy = prompt('Digita que dia de la semana es, primera letra en mayusculas, sin punto, ni caracteres especiales, ni espacios: ')
+let hoy = prompt('Digita que dia de la semana es, primera letra en mayusculas, sin punto, ni caracteres especiales, ni espacios: ');
 
 if (hoy == "Sabado" | hoy == "Domingo") {
     
@@ -59,11 +59,11 @@ if (hoy == "Sabado" | hoy == "Domingo") {
     
     alert('Que rinda la semana.......')
 
-}
+};
 
 /* 2. Verifica si un número ingresado por el usuario es positivo o negativo. Muestra una alerta informativa. */
 
-let numero = prompt('Digita un numero, para evaluar si es 0, positivo o negativo')
+let numero = prompt('Digita un numero, para evaluar si es 0, positivo o negativo');
 
 if (numero > 0) {
     alert('Tu numero es positivo: ' + numero)
@@ -73,7 +73,7 @@ if (numero > 0) {
     } else {
         alert(`el numero que digitaste es ${numero} 0`)
     }
-}
+};
 
 /* 3. Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has
 ganado!". En caso contrario, muestra "Intentalo nuevamente para ganar.". */
@@ -81,4 +81,12 @@ ganado!". En caso contrario, muestra "Intentalo nuevamente para ganar.". */
 /* 4. Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor
 del saldo. */
 
+let mensaje = "Tu cuenta ha sido baneada por infringir los terminos de uso y condiciones"
+
+alert(`Mensaje del sistema: ${mensaje}`);
+
 /* 5. Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.*/
+
+let userName = prompt('digita tu nombre: ');
+
+alert(`Bienvenid@ ${userName} el sistema está en linea`)
