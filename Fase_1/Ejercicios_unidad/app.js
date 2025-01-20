90eb67283a5f6f6b5904997d72309698e8022c5a
ojsc <ojsilvera@gmail.com>
2025-01-20 16:03:48 -0500

    ejercicios de parctica 2

--- Fase_1/Ejercicios_unidad/app.js
+++ Fase_1/Ejercicios_unidad/app.js
@@ -33,13 +33,52 @@ alert(mensajeDeError);
 
 // 8. Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
 
-let nombreUsuario = prompt("Nomre usuario: ")
+let nombreUsuario = prompt("Nombre usuario: ");
 
 /* 9. Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad. Ahora, si la edad es mayor
 o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!". */
 
-let edadUsuario = prompt("Digita tu edad: ")
+let edadUsuario = prompt("Digita tu edad: ");
 
 if (edadUsuario >= 18) {
     alert("¡Puedes obtener tu licencia de conducir!")
+};
+
+// Desafíos 2:
+
+/* 1. Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!".
+De lo contrario, muestra "¡Buena semana!". */
+
+let hoy = prompt('Digita que dia de la semana es, primera letra en mayusculas, sin punto, ni caracteres especiales, ni espacios: ')
+
+if (hoy == "Sabado" | hoy == "Domingo") {
+    
+    alert('Que bien es fin de semana')
+
+} else {
+    
+    alert('Que rinda la semana.......')
+
 }
+
+/* 2. Verifica si un número ingresado por el usuario es positivo o negativo. Muestra una alerta informativa. */
+
+let numero = prompt('Digita un numero, para evaluar si es 0, positivo o negativo')
+
+if (numero > 0) {
+    alert('Tu numero es positivo: ' + numero)
+} else {
+    if (numero < 0) {
+        alert('Tu numero es negativo: ' + numero)
+    } else {
+        alert(`el numero que digitaste es ${numero} 0`)
+    }
+}
+
+/* 3. Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has
+ganado!". En caso contrario, muestra "Intentalo nuevamente para ganar.". */
+
+/* 4. Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor
+del saldo. */
+
+/* 5. Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.*/
--- Fase_1/Indice_Fase_1.md
+++ Fase_1/Indice_Fase_1.md
@@ -89,3 +89,22 @@ nos permite insertar un valor dinámico en la cadena. Todo lo que hay dentro ${}
 
 Por ejemplo, podríamos escribir Se estima que la Tierra tiene ${edad + 10} mil millones de años., y funcionaría como si
 hiciéramos const edad = 4.5 + 10;.
+
+Tambien es posible concatenar de la siguiente manera:
+
+    alert('un texto ' + variable1 + ' ' + 'otro texto ' + variable2)
+
+Desafíos:
+
+Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!". De
+lo contrario, muestra "¡Buena semana!".
+
+Verifica si un número ingresado por el usuario es positivo o negativo. Muestra una alerta informativa.
+
+Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has ganado!".
+En caso contrario, muestra "Intentalo nuevamente para ganar.".
+
+Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor
+del saldo.
+
+Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
