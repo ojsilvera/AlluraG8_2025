0bd0cce81d234f3d556220bebae74125b4c2f208
ojsc <ojsilvera@gmail.com>
2025-01-20 15:00:02 -0500

    estructura de decision si sino

--- Fase_1/AdivinaNumero/app.js
+++ Fase_1/AdivinaNumero/app.js
@@ -2,10 +2,10 @@
 let numeroSecreto = 6;
 
 // carga el numero digitado por el usuario en una variable llamada numeroUsuario
-let numeroUsuario = prompt("Digita un numero: ");
+let numeroUsuario = prompt("Digita un numero entre 1 y 10: ");
 
 // muestra el contenido de la variable en la consola del navegador
-console.log(numerUsuario);
+console.log(numeroUsuario);
 
 //muestra el contenido de la variable en una alerta del navegador
 alert(numeroUsuario);
@@ -15,4 +15,11 @@ alert(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto) {
     alert('Has acertado El numero secreto es ${numeroSecreto}');
+} else {
+    alert("No acertaste el numero secreto")
+    if (numeroUsuario < numeroSecreto) {
+        alert("tu numero es menor que el numero secreto")
+    } else {
+        alert("Tu numero es mayor que el numero secreto")
+    }
 }
