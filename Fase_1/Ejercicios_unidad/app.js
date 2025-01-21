61d1af89066e3c192e4d1078434aa239cfaa251f
ojsc <ojsilvera@gmail.com>
2025-01-21 15:56:08 -0500

    Completando los ejercicios capitulo 2

--- Fase_1/Ejercicios_unidad/app.js
+++ Fase_1/Ejercicios_unidad/app.js
@@ -78,6 +78,14 @@ if (numero > 0) {
 /* 3. Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has
 ganado!". En caso contrario, muestra "Intentalo nuevamente para ganar.". */
 
+let puntaje = prompt('Digita tu puntaje');
+
+if (puntaje >= 100) {
+    alert('¡Felicidades, has ganado!')
+} else {
+    alert('Intentalo nuevamente para ganar.')
+}
+
 /* 4. Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor
 del saldo. */
 
