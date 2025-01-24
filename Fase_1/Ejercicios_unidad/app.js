2f923d0ecee7683ba0bdb83d63e77eacdfc99458
ojsc <ojsilvera@gmail.com>
2025-01-23 21:04:32 -0500

    Reto 3

--- Fase_1/Ejercicios_unidad/app.js
+++ Fase_1/Ejercicios_unidad/app.js
@@ -1,4 +1,4 @@
-// Ejercicios capitulo 1
+// Desafio 1::---------------------------------------------------------------------------------------------------------
 
 // 1. Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
 
@@ -44,7 +44,7 @@ if (edadUsuario >= 18) {
     alert("¡Puedes obtener tu licencia de conducir!")
 };
 
-// Desafíos 2:
+// Desafíos 2:---------------------------------------------------------------------------------------------------------
 
 /* 1. Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!".
 De lo contrario, muestra "¡Buena semana!". */
@@ -151,3 +151,56 @@ while (contadorProgresivo > incremento) {
     incremento = incremento + 1;
     
 };
+
+// Desafio 3::---------------------------------------------------------------------------------------------------------
+
+/*Crea un programa que utilice console.log para mostrar un mensaje de bienvenida.*/
+
+console.log("Bienvenidos");
+
+/*Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza console.log para mostrar el mensaje "¡Hola,
+[tu nombre]!" en la consola del navegador.*/
+
+let miNombre = "Oscar";
+console.log(miNombre);
+
+/*Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza alert para mostrar el mensaje "¡Hola, [tu nombre]!".*/
+
+let miNombreAlert = "Oscar";
+alert(`¡Hola, ${miNombreAlert}!`);
+
+/*Utiliza prompt y haz la siguiente pregunta: ¿Cuál es el lenguaje de programación que más te gusta?. Luego, almacena la
+respuesta en una variable y muestra la respuesta en la consola del navegador.*/
+
+let lenguaje = prompt('¿Cuál es el lenguaje de programación que más te gusta?');
+console.log(lenguaje);
+
+/*Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza
+la suma de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log para
+mostrar el mensaje "La suma de [valor1] y [valor2] es igual a [resultado]." en la consola.*/
+
+let valor1 = 15;
+let valor2 = 25;
+let resultado = valor1 + valor2;
+console.log(`La suma de ${valor1} y ${valor2} es igual a ${resultado}`);
+
+/*Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza
+la resta de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log
+para mostrar el mensaje "La diferencia entre [valor1] y [valor2] es igual a [resultado]." en la consola.*/
+
+/*Pide al usuario que ingrese su edad con prompt. Con base en la edad ingresada, utiliza un if para verificar si la persona
+es mayor o menor de edad y muestra un mensaje apropiado en la consola.*/
+
+/*Crea una variable "numero" y solicita un valor con prompt. Luego, verifica si es positivo, negativo o cero utilizando un
+if-else y muestra el mensaje correspondiente.*/
+
+/*Utiliza un bucle while para mostrar los números del 1 al 10 en la consola.*/
+
+/*Crea una variable "nota" y asígnale un valor numérico. Utiliza un if-else para determinar si la nota es mayor o igual a 7
+y muestra "Aprobado" o "Reprobado" en la consola.*/
+
+/*Utiliza Math.random para generar cualquier número aleatorio y muestra ese número en la consola.*/
+
+/*Utiliza Math.random para generar un número entero entre 1 y 10 y muestra ese número en la consola.*/
+
+/*Utiliza Math.random para generar un número entero entre 1 y 1000 y muestra ese número en la consola.*/
