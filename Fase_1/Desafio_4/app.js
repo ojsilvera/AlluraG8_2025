292c21a13396cbccc3e1a721a1ce8c5c895686f8
ojsc <ojsilvera@gmail.com>
2025-02-01 22:29:16 -0500

    desarrollo desafio 4

--- Fase_1/Desafio_4/app.js
+++ Fase_1/Desafio_4/app.js
@@ -1,1 +1,18 @@
+// cambia el contenido de la etiqueta h1 por "Hora del Desafío"
 
+let titleUser = document.querySelector('h1');
+titleUser.innerHTML = 'Hora del Desafío';
+
+// Crea una función que muestre en la consola el mensaje "El botón fue clicado" siempre que se presione el botón "Console".
+
+function consoleUser(){
+    console.log('El botón fue clicado');
+}
+
+/* Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil.
+Luego, muestra una alerta con el mensaje, concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti". */
+
+function prompUser(){
+    let city = prompt('Escribe el nombre de una ciudad de Brasil');
+    alert(`Estuve en ${city} y me acordé de ti`);
+}
